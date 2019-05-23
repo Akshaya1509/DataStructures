ff8a75c6d3b4be7d22f3dcd3f249738947ab10c3
Akshaya <akshayamukundhan@gmail.com>
2019-05-23 07:35:18 +0530

    Chap8 - initial commit

---
+++ chap8-recursion-and-dp/scripts/triple_hop.py
@@ -0,0 +1,17 @@
+# Script to check how many ways child will run in n steps in a staircase
+# TODO: check logic and ans and checkin 
+
+def n_steps(n, memo):
+    # print(n)
+    if n < 0:
+        return 0
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n not in memo:
+        memo[n] = n_steps([n-1][0], memo) + n_steps([n-2][0], memo) + n_steps([n-3][0], memo)
+    return memo[n]
+
+memo = {}
+print(n_steps(10, memo))
